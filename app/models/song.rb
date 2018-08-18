31cc62ce9ffdcee045b2a8fee5ca331c15aa94c0
Veronica Bustabad <vbustabad@gmail.com>
2018-08-18 13:56:46 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,8 +6,7 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    artist = Artist.find_by(name: name)
+    artist = Artist.find_or_create_by(name: name)
     self.artist = artist
-    self.artist.save
   end
 end
